3e74ab0c06b24bbdf712a70acae311e23432417e
matthewogtong <ogtongm@gmail.com>
2021-07-08 03:14:18 -0400

    Working with Union types.

--- session-2/app.js
+++ session-2/app.js
@@ -1,57 +1,16 @@
-// ** EXPLICIT OBJECT - example of explicitly defining an object
-// const person: {
-//     name: string;
-//     age: number;
-// } = {}
-// ** ARRAY AND TUPLE - example of using an array and tuple in typescript
-// const person: {
-//     name: string;
-//     age: number;
-//     hobbies: string[];
-//     role: [number, string];
-// } = {
-//   name: "Matthew",
-//   age: 30,
-//   hobbies: ['Sports', 'Cooking'],
-//   role: [2, 'author']
-// };
-// ** ENUM - example where you should implement enum instead
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-var Role;
-(function (Role) {
-    Role["ADMIN"] = "ADMIN";
-    Role[Role["READ_ONLY"] = 100] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 101] = "AUTHOR";
-})(Role || (Role = {}));
-;
-var person = {
-    name: "Matthew",
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.AUTHOR
-};
-// person.role.push('admin');
-// person.role[1] = 10;
-var favoriteActivities;
-favoriteActivities = ['Sports'];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
-    // console.log(hobby.map()); // Error
+// Union type
+// * useful to use on a parameter of a function where we accept two different kind of values
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
-if (person.role === Role.AUTHOR) {
-}
-switch (person.role) {
-    case Role.ADMIN:
-        console.log('you have admin privileges');
-        break;
-    case Role.READ_ONLY:
-        console.log('you have read only privileges');
-        break;
-    case Role.AUTHOR:
-        console.log('you are an author');
-        break;
-}
+var combinedAges = combine(30, 26);
+console.log(combinedAges);
+var combinedNames = combine("Max", "Anna");
+console.log(combinedNames);
