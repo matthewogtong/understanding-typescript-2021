b43404bec0bc2bf3143dc321576e60f816836c8c
matthewogtong <ogtongm@gmail.com>
2021-07-08 02:49:41 -0400

    Create new app.ts file and rename previous one to objs-arrays-enum

--- session-2/objs-arrays-enum.ts
+++ session-2/objs-arrays-enum.ts
@@ -3,6 +3,7 @@
 //     name: string;
 //     age: number;
 // } = {}
+
 // ** ARRAY AND TUPLE - example of using an array and tuple in typescript
 // const person: {
 //     name: string;
@@ -15,35 +16,39 @@
 //   hobbies: ['Sports', 'Cooking'],
 //   role: [2, 'author']
 // };
+
+
 // ** ENUM - example where you should implement enum instead
 // const ADMIN = 0;
 // const READ_ONLY = 1;
 // const AUTHOR = 2;
-var Role;
-(function (Role) {
-    Role["ADMIN"] = "ADMIN";
-    Role[Role["READ_ONLY"] = 100] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 101] = "AUTHOR";
-})(Role || (Role = {}));
-;
-var person = {
-    name: "Matthew",
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.AUTHOR
+
+enum Role { ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR };
+
+const person = {
+  name: "Matthew",
+  age: 30,
+  hobbies: ['Sports', 'Cooking'],
+  role: Role.AUTHOR
 };
+
 // person.role.push('admin');
 // person.role[1] = 10;
-var favoriteActivities;
+
+let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
+
 console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
+
+for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
     // console.log(hobby.map()); // Error
 }
+
 if (person.role === Role.AUTHOR) {
+    
 }
+
 switch (person.role) {
     case Role.ADMIN:
         console.log('you have admin privileges');
