670931d8a7f1f516cd411caadd7005f22a7b91f6
matthewogtong <ogtongm@gmail.com>
2021-08-11 18:01:48 -0400

    insert third argument for combine function, implementing literal types

--- 02-typescript-basics-and-basic-types/app.js
+++ 02-typescript-basics-and-basic-types/app.js
@@ -1,9 +1,10 @@
 // Union type
 // * useful to use on a parameter of a function where we accept two different kind of values
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultConversion === "as-number") {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
